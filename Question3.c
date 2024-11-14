8f284e9028bef2f0b8d3f6c44587127f4c24543a
lukacassar <luka.cassar.24@um.edu.mt>
2024-11-14 22:17:07 +0100

    fixed

--- Question3.c
+++ Question3.c
@@ -2,14 +2,25 @@
 
 int main(void)
 {
-    printf("PIN is 9999 \n");
+    int PIN = 9999;
+    int entry;
+    printf("PIN is %d \n", PIN);
     int n = 5;
 
-    while(n!=0 {
+    while(n!=0) {
     printf("Enter PIN\n");
-        
+        scanf("%d",&entry);
+        if(entry != PIN) {
+            printf("Wrong PIN\n");
+            n--;
+        } else {
+            printf("Thank you for using this program\n");
+            return 0;
+        }
     }
-
+    if (n == 0) {
+        printf("You have been locked out");
+    }
 
     return 0;
 }
